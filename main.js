83ba6b5acfd994b4f6a6ac78cbe63154ed203798
MickeMarten <KarlMikael.Martensson@gmail.com>
2023-11-08 23:26:11 +0100

    Improved JS-code

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const mobileMenuContainer = document.querySelector('#mobileMenuContainer__ID');
 const menuBtn = document.querySelector('#header__menubutton');
 const exitBtn = document.querySelector('#MobileMenuContainer__ExitBtn');
-menuBtn.addEventListener('click', function() {
-mobileMenuContainer.classList.toggle('mobileMenuContainer');
-mobileMenuContainer.classList.remove('mobileMenuContainer__hidden');
-});
-exitBtn.addEventListener('click', function(){
-mobileMenuContainer.classList.toggle('mobileMenuContainer__hidden');
-mobileMenuContainer.classList.remove('mobileMenuContainer');  
-})
+//menuBtn.addEventListener('click', function() {
+//mobileMenuContainer.classList.toggle('mobileMenuContainer');
+//mobileMenuContainer.classList.remove('mobileMenuContainer__hidden');
+//});
+//exitBtn.addEventListener('click', function(){
+//mobileMenuContainer.classList.toggle('mobileMenuContainer__hidden');
+//mobileMenuContainer.classList.remove('mobileMenuContainer');  
+//})
+
+ function toggleMobileMenu () {
+    mobileMenuContainer.classList.toggle('mobileMenuContainer');
+    mobileMenuContainer.classList.toggle('mobileMenuContainer__hidden');
+
+}
+
+menuBtn.addEventListener('click', toggleMobileMenu);
+exitBtn.addEventListener('click', toggleMobileMenu);
+
 
 
 
